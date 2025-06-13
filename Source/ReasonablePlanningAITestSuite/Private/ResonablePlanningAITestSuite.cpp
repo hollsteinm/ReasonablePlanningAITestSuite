cf3ba9b2ea5d1048540301d6547865d06e2babf8
Martin Hollstein <hollsteinm@users.noreply.github.com>
2025-06-12 21:03:03 -0500

    first commit

---
+++ Source/ReasonablePlanningAITestSuite/Private/ResonablePlanningAITestSuite.cpp
@@ -0,0 +1,13 @@
+// Copyright Radaway Software LLC. 2025. All rights reserved.
+
+#include "ReasonablePlanningAITestSuite.h"
+
+#define LOCTEXT_NAMESPACE "ResonablePlanningAITestSuite"
+
+struct FReasonablePlanningAITestSuiteModule : public IReasonablePlanningAITestSuiteModule
+{
+};
+
+IMPLEMENT_MODULE(FReasonablePlanningAITestSuiteModule, ReasonablePlanningAITestSuite)
+
+#undef LOCTEXT_NAMESPACE
